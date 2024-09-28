d92dce378950a50e6536a8e1923e4a80859f4a16
yazid <muhammad.baihaqi@solusi247.com>
2024-09-28 12:33:27 +0700

    update 2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -77,9 +77,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
           VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
-        // If a database error occurs, return a more specific error.
         return {
-            message: 'Database Error: Failed to Create Invoice.',
+            message: `Database Error: Failed to Create Invoice. ${error}`,
         };
     }
 
@@ -111,7 +110,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
             WHERE id = ${id}
           `;
     } catch (error) {
-        return { message: 'Database Error: Failed to Update Invoice.' };
+        return { message: `Database Error: Failed to Update Invoice. ${error}` };
     }
 
     revalidatePath('/dashboard/invoices');
@@ -125,6 +124,6 @@ export async function deleteInvoice(id: string) {
         revalidatePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
     } catch (error) {
-        return { message: 'Database Error: Failed to Delete Invoice.' };
+        return { message: `Database Error: Failed to Delete Invoice. ${error}` };
     }
 }
